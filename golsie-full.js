35a550a7683dfea531392f975f5e1913fbaec11a
shahafOne <shahafw1990@gmail.com>
2026-01-19 17:11:16 +0200

    added custom modal - header fix 5

--- golsie-full.js
+++ golsie-full.js
@@ -2085,9 +2085,17 @@ document.addEventListener("DOMContentLoaded", function() {
         dynamicContent.innerHTML = '';
         dynamicContent.appendChild(contentToMove);
 
-        window.scrollTo(0, savedScrollY);
-        document.body.style.top = -savedScrollY + 'px';
-        console.log("asdsadasd");
+        requestAnimationFrame(function() {
+          window.scrollTo(0, savedScrollY);
+          document.body.style.top = -savedScrollY + 'px';
+          console.log("asdsadasd");
+          // Double-check after another frame to be absolutely sure
+          requestAnimationFrame(function() {
+            window.scrollTo(0, savedScrollY);
+            document.body.style.top = -savedScrollY + 'px';
+          });
+        });
+        
         // FIX: Remove disabled attribute from submit buttons after moving
         // Webflow adds this in hidden forms, but we want it enabled in modal
         setTimeout(function() {
